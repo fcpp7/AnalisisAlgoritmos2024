dfec5bf1dcecc93cc7721b168d82c1e992740dc9
fcpp7 <facundo.perez@est.fi.uncoma.edu.ar>
2024-08-15 23:39:59 -0300

    Primos

---
+++ ejericios/Ejercicio_3_Primos.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Ejercicio_3_Primos {
+
+	public static boolean chequearPrimo(int numero){
+		/* Iniciamos un boolean en true */
+		boolean esPrimo = true; 
+		if (numero <= 1){
+			esPrimo = false;
+		} else if (numero <= 3){
+			esPrimo = true;
+		} else if (numero % 2 == 0 || numero % 3 == 0){
+			esPrimo = false;
+		} else {        
+	    int i = 5;
+			while (i * i <= numero){
+				if (numero % i == 0 || numero % (i + 2) == 0){
+					esPrimo = false;
+				}
+				i += 6;
+			}
+		}
+
+		return esPrimo;
+
+
+	}
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingresar un número entero: ");
+        int n = scanner.nextInt();
+        
+        int primos = 0;
+        
+        // Contamos los números primos entre 1 y n
+        for (int i = 1; i <= n; i++) {
+            if (chequearPrimo(i)) {
+                primos++;
+
+            }
+        }
+        
+        System.out.println("Entre 1 y " + n + " hay " + primos + " números primos");
+        
+        scanner.close();
+    }
+}
